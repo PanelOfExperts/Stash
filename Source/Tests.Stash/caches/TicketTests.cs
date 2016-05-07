945cd37da76fcf8e910b327bf2d29b19477470fb
Seth <sgt.mccarthy@gmail.com>
2016-05-07 10:17:41 -0700

    TicketTests describe Ticket behavior more fully

--- Source/Tests.Stash/caches/TicketTests.cs
+++ Source/Tests.Stash/caches/TicketTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using NUnit.Framework;
 using Stash.caches;
 using Stash.rules;
@@ -10,20 +11,51 @@ namespace Stash.Test.caches
     class TicketTests
     {
         [Test]
-        public void Key_Test()
+        public void PropertiesOnConstruction()
         {
-            var expected = Path.GetRandomFileName();
-            var ticket = new Ticket(expected, null);
+            var key = Path.GetRandomFileName();
+            var value = new object();
+            var beforeCreationTime = DateTime.UtcNow;
+            var ticket = new Ticket(key, value);
+            var afterCreationTime = DateTime.UtcNow;
 
-            Assert.AreEqual(expected, ticket.Key);
+            Assert.That(ticket.Created, Is.GreaterThanOrEqualTo(beforeCreationTime).And.LessThanOrEqualTo(afterCreationTime));
+            Assert.That(ticket.LastAccessed, Is.EqualTo(ticket.Created));
+            Assert.AreEqual(key, ticket.Key);
+            Assert.AreEqual(value, ticket.Value);
+        }
+
+        [Test]
+        public void LastAccessIsUpdatedOnGet()
+        {
+            var ticket = new Ticket(Guid.NewGuid().ToString(), new object());
+
+            var original = ticket.LastAccessed;
+            Thread.Sleep(5); //to make sure we move enough ticks for the resolution of our measurements to notice
+            var ignore = ticket.Value;
+            
+            Assert.That(ticket.LastAccessed, Is.GreaterThan(original));
+        }
+
+        [Test]
+        public void LastAccessedNotUpdatedOnCheckingOtherProperties()
+        {
+            var ticket = new Ticket(Guid.NewGuid().ToString(), new object());
+
+            var original = ticket.LastAccessed;
+            Thread.Sleep(5); //to make sure we move enough ticks for the resolution of our measurements to notice
+            var ignore1 = ticket.Key;
+            var ignore2 = ticket.Created;
+            var ignore3 = ticket.LastAccessed;
+
+            Assert.That(ticket.LastAccessed, Is.EqualTo(original));
         }
 
         [Test]
         public void ConstructorWithNullOrEmptyKey_Throws()
         {
-            Ticket ticket = null;
-            Assert.Throws<ArgumentException>(()=> ticket = new Ticket(null, null));
-            Assert.Throws<ArgumentException>(() => ticket = new Ticket(string.Empty, null));
+            Assert.Throws<ArgumentException>(()=> new Ticket(null, null));
+            Assert.Throws<ArgumentException>(() => new Ticket(string.Empty, null));
         }
     }
 }
